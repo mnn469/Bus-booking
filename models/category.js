879dc127a48429cc81d4303d70a330bd71b96a24
mnn469 <89789273+mnn469@users.noreply.github.com>
2021-09-13 12:11:55 +0530

    Update category.js

--- models/category.js
+++ models/category.js
@@ -1,10 +1,5 @@
 module.exports = (sequelize, DataTypes)=>{
     const category = sequelize.define("category", {
-        // busId: {
-        //     type: DataTypes.INTEGER,
-        //     allowNull : false,
-        //     primaryKey : true
-        // },
         name: {
             type: DataTypes.TEXT,
             allowNull : false
